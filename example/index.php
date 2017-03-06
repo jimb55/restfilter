21423a374b3684e0d5d338c718352d017806559e
jimb55 <760785539@qq.com>
2017-03-06 14:23:59 +0800

    修改example和依赖包

--- example/index.php
+++ example/index.php
@@ -1,19 +1,20 @@
 <?php
 
-require __DIR__."/../vendor/autoload.php";
+require __DIR__ . "/../vendor/autoload.php";
 
 use League\Fractal\Manager;
 use League\Fractal\Resource\Collection;
+use League\Fractal\Resource\Item;
 use Jimb\RestFilter\CommonTransformer;
 
-// Create a top level instance somewhere
+// 新建一个全局管理类
 $fractal = new Manager();
 
 $books = [
     [
         'id' => '1',
         'title' => 'Hogfather',
-        'name'  => 'jiabi',
+        'name' => 'jiabi',
         'yr' => '1998',
         'author_name' => 'Philip K Dick',
         'author_email' => 'philip@example.org',
@@ -22,41 +23,34 @@ $books = [
         'id' => '2',
         'title' => 'Game Of Kill Everyone',
         'yr' => '2014',
-        'name'  => '223',
+        'name' => '223',
         'author_name' => 'George R. R. Satan',
         'author_email' => 'george@example.org',
         'myinfo' => [
             'name' => 'jimb55',
-            'githubPage'  => 'github://jimb55',
-            'age'  =>  '22',
-            'like'  => 'i down no'
+            'githubPage' => 'github://jimb55',
+            'age' => '22',
+            'like' => 'i down no'
         ]
     ]
 ];
 
+//or
 
-//$resource = new Collection($books, function(array $book) {
-//    return [
-//        'id'      => (int) $book['id'],
-//        'title'   => $book['title'],
-//        'year'    => (int) $book['yr'],
-//        'author'  => [
-//            'name'  => $book['author_name'],
-//            'email' => $book['author_email'],
-//        ],
-//        'links'   => [
-//            [
-//                'rel' => 'self',
-//                'uri' => '/books/'.$book['id'],
-//            ]
-//        ]
-//    ];
-//});
+//$book = [
+//    'id' => '1',
+//    'title' => 'Hogfather',
+//    'name' => 'jiabi',
+//    'yr' => '1998',
+//    'author_name' => 'Philip K Dick',
+//    'author_email' => 'philip@example.org',
+//];
 
-$resource = new Collection($books, new CommonTransformer );
+//列表
+$resource = new Collection($books, new CommonTransformer);
 
-// Turn that into a structured array (handy for XML views or auto-YAML converting)
-//$array = $fractal->createData($resource)->toArray();
+//单项
+//$resource = new Item($book, new CommonTransformer);
 
-// Turn all of that into a JSON string
+// 返回
 echo $fractal->createData($resource)->toJson();
